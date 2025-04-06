ed9accc183d08b0b437f7dc515fb6f82245c1e6b
hectormeyi2 <hectormeyi2@gmail.com>
2025-04-06 13:47:34 -0500

    elimine los comentarios servidor Express+TypeScript

--- dist/app.js
+++ dist/app.js
@@ -16,8 +16,4 @@ app.listen(PORT, () => {
 }).on("error", (error) => {
     throw new Error(error.message);
 });
-// en el punto 8 me salio este error despues de mandar el comando npx tsc -w
-// no aparece como en la imagen como si el servidor estuviera corriendo
-// el paso nuevo habla de la segunda terminal
-// como se saca la nueva terminal no importa que sea powershell 
-// sale un mensaje servidor ejecutandose en el puerto 10101
+
